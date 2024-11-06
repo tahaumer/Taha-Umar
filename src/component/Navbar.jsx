39ff17074fcb33bae3b52cfb2e1450ae9bc4dff9
Taha <135301932+tahaumer@users.noreply.github.com>
2024-11-07 00:20:19 +0500

    Update Navbar.jsx

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -6,8 +6,8 @@ const Navbar = () => {
     <div className='container p-5 bg-transparent flex justify-between items-center '>
       <img src={logo} alt='Taha' className='w-16 ' />
       <div className='font-firaCode font-bold text-lg'>
-        <button className='block border-b border-neutral1 text-secondary'>EN</button>
-        <button className='block text-neutral1'>DE</button>
+        <button className='block text-secondary'>EN</button>
+{/*         <button className='block text-neutral1'>DE</button> */}
       </div>
     </div>
 
